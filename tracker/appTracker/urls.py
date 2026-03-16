2212d72e839ac85f622a79101d44bd1056a9c473
Ayush-OP00 <allinone7925@gmail.com>
2026-03-16 13:41:27 +0530

    This commit adds the login.html and its configuration to views.py and urls.py. The result is that the webpage is running both the templates just while enterning the data I am getting the operational error.

--- tracker/appTracker/urls.py
+++ tracker/appTracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path  
-from appTracker.views import registration
+from appTracker.views import registration, login_view
 
 urlpatterns = [
     path('', registration, name = 'reg'),
+    path('login', login_view, name ='login'),
 ]
--- tracker/appTracker/views.py
+++ tracker/appTracker/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user=authenticate(request, username=username, password=password)
+        print (user, username, password)
+        if user is not None:
+            login(request, user)
+            return  redirect('home')
+    return render(request, 'login.html')
 def registration(request):
     if request.method == "POST":
-        name = request.POST['name']
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
 
-        user = User.objects.create_user(name = name, username=username, email = email, password=password )
+        user = User.objects.create_user(username=username, email = email, password=password )
         user.save()
         
         return redirect('login')
     return render(request, 'reg.html')  
+
+
