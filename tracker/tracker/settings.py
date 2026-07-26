77d0fc616523459c57501ed5f52965dd84b3d9f2
Ayush-OP00 <allinone7925@gmail.com>
2026-07-27 04:38:19 +0530

    Configure production hosts and settings

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com",]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
 
 # Application definition
